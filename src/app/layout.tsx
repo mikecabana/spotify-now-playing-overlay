740ca05c96c40426733106ab36ba9fb9be573627
mikecabana <mikeyc343@gmail.com>
2023-11-22 14:15:21 -0500

    fixes layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
-import { Inter } from 'next/font/google';
 import { GeistSans } from 'geist/font/sans';
 import { Toaster } from 'react-hot-toast';
 import './globals.css';
 import type { Metadata } from 'next';
 import Script from 'next/script';
 
-const inter = Inter({ subsets: ['latin'] });
-
-const host = 'https://spotify-now-playing-overlay.vercel.app';
-
 export const metadata: Metadata = {
-    metadataBase: new URL(`${host}`),
     title: 'Spotify Now Playing Overlay',
     description:
         'Display your currently playing track on Spotify. Embed in your websites or add to your stream overlays.',
@@ -18,15 +12,7 @@ export const metadata: Metadata = {
         title: 'Spotify Now Playing Overlay',
         description:
             'Display your currently playing track on Spotify. Embed in your websites or add to your stream overlays.',
-        url: `${host}`,
         type: 'website',
-        images: [
-            {
-                url: `${host}/opengraph-image.png`,
-                width: 1200,
-                height: 630,
-            },
-        ],
     },
     twitter: {
         card: 'summary_large_image',
