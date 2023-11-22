de665c215bc1837f4f99d9ac5752d97338fbbbf9
mikecabana <mikeyc343@gmail.com>
2023-11-22 14:06:29 -0500

    adds analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,14 @@ import { GeistSans } from 'geist/font/sans';
 import { Toaster } from 'react-hot-toast';
 import './globals.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const host = 'https://spotify-now-playing-overlay.vercel.app';
+
 export const metadata: Metadata = {
-    metadataBase: new URL(`https://spotify-now-playing-overlay.vercel.app`),
+    metadataBase: new URL(`${host}`),
     title: 'Spotify Now Playing Overlay',
     description:
         'Display your currently playing track on Spotify. Embed in your websites or add to your stream overlays.',
@@ -15,8 +18,15 @@ export const metadata: Metadata = {
         title: 'Spotify Now Playing Overlay',
         description:
             'Display your currently playing track on Spotify. Embed in your websites or add to your stream overlays.',
-        url: 'https://spotify-now-playing-overlay.vercel.app',
+        url: `${host}`,
         type: 'website',
+        images: [
+            {
+                url: `${host}/opengraph-image.png`,
+                width: 1200,
+                height: 630,
+            },
+        ],
     },
     twitter: {
         card: 'summary_large_image',
@@ -28,11 +38,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <body className={GeistSans.className}>
-                {children}
-                <Toaster position="top-center" />
-            </body>
-        </html>
+        <>
+            <Script
+                async
+                src="https://analytics.eu.umami.is/script.js"
+                data-website-id="8c33b96a-3845-4323-b8f1-457d1403ace4"
+            />
+            <html lang="en">
+                <body className={GeistSans.className}>
+                    {children}
+                    <Toaster position="top-center" />
+                </body>
+            </html>
+        </>
     );
 }
