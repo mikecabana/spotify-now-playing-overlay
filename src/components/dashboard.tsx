311bcdb28a8de1d66b00fd0c508bc9b64cd672ee
mikecabana <mikeyc343@gmail.com>
2025-02-10 21:41:42 -0500

    fixed environment check for scheme

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,24 +1,11 @@
 import { Session } from "next-auth";
-import { headers as nextHeaders } from "next/headers";
 import { UrlBuilder } from "./url-builder";
 
 type DashboardProps = { domain: string; session: Session };
 
 export async function Dashboard({ domain, session }: DashboardProps) {
-  const headers = await nextHeaders();
-  const schemeHeader = headers.get(":scheme:");
-  const refererHeader = headers.get("referer");
-
-  let scheme: "https" | "http" = "http";
-
-  if (refererHeader) {
-    const url = new URL(refererHeader);
-    scheme = url.protocol === "https:" ? "https" : "http";
-  }
-
-  if (schemeHeader) {
-    scheme = schemeHeader as "https" | "http";
-  }
+  const scheme: "https" | "http" =
+    process.env.VERCEL_ENV === "production" ? "https" : "http";
 
   return (
     <div>
